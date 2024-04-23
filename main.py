0574684e880e646c7807c6d157a5332666b8f802
Arthur Rampani <arthurrampanicosta@gmail.com>
2024-04-23 08:24:53 -0300

    Add:

---
+++ main.py
@@ -0,0 +1,11 @@
+#### JOGOTECA ####
+# DESENVOLVIDO POR ARTHUR RAMPANI #
+
+print("*****************************")
+print("*    BEM VINDO À JOGOTECA   *")
+print("*          JOGOS            *")
+print("*    JOGO DA ADVINHAÇÃO     *")
+print("*     JOGO DA FORCA         *")
+print("*     JOGO DA VELHA         *")
+print("*    TABUADA EXTREME        *")
+print("*****************************")
