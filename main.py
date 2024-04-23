735bbf5350874d8382bb1f2211bad6ee37766b14
Arthur Rampani <arthurrampanicosta@gmail.com>
2024-04-23 08:41:44 -0300

    Add: Validação por idade

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #### JOGOTECA ####
 # DESENVOLVIDO POR ARTHUR RAMPANI #
 
-print("*****************************")
-print("*    BEM VINDO À JOGOTECA   *")
-print("*          JOGOS            *")
-print("*    JOGO DA ADVINHAÇÃO     *")
-print("*     JOGO DA FORCA         *")
-print("*     JOGO DA VELHA         *")
-print("*    TABUADA EXTREME        *")
-print("*****************************")
-
 pessoa = int(input("Qual a sua idade? "))
 if pessoa >=18:
     print("Você está liberado para jogar")
+    print("*****************************")
+    print("*    BEM VINDO À JOGOTECA   *")
+    print("*          JOGOS            *")
+    print("*   1- JOGO DA ADVINHAÇÃO   *")
+    print("*   2-  JOGO DA FORCA       *")
+    print("*   3-  JOGO DA VELHA       *")
+    print("*   4- TABUADA EXTREME      *")
+    print("*****************************")
 else:
     print("Sai daqui muleke")
