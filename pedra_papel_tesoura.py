e0ed867e94f05294cc0f90fb02359d68d9aaee14
Arthur Rampani <athurrampanicosta@gmail.com>
2024-04-29 15:29:02 -0300

    Add: Fazendo os números virar PEDRA,PAPEL,TESOURA

--- pedra_papel_tesoura.py
+++ pedra_papel_tesoura.py
@@ -5,3 +5,15 @@ print("2 - PAPEL")
 print("3 - TESOURA")
 numero_escolhido = int(input("digite um número: "))
 numero_do_pc = random.randint(1,3)
+if numero_do_pc == 1:
+    numero_do_pc = "PEDRA"
+if numero_do_pc == 2:
+    numero_do_pc = "PAPEL"
+if numero_do_pc == 3:
+    numero_do_pc = "TESOURA"
+if numero_escolhido == 1:
+    numero_escolhido = "PEDRA"
+if numero_escolhido == 2:
+    numero_escolhido = "PAPEL"
+if numero_escolhido == 3:
+    numero_escolhido = "TESOURA"
